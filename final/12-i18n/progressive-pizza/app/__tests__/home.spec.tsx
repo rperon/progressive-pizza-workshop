2845753deedbf41b949a4c1da250faff3bfb6b1c
Rémi Peron <remi.peron@gmail.com>
2024-09-27 14:14:30 +0200

    fix tests

--- final/12-i18n/progressive-pizza/app/__tests__/home.spec.tsx
+++ final/12-i18n/progressive-pizza/app/__tests__/home.spec.tsx
@@ -3,6 +3,9 @@ import { default as Component, action } from "~/routes/_index";
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { I18nextProvider, initReactI18next } from "react-i18next";
+import i18next from "i18next";
+import * as i18n from "../i18n";
 
 const App = createRemixStub([
   {
@@ -19,9 +22,14 @@ const App = createRemixStub([
 ]);
 
 test("rendering the order page", async () => {
-  render(<App initialEntries={["/"]} initialIndex={0} />);
+  render(<App initialEntries={["/"]} initialIndex={0} />, {
+    wrapper: ({ children }) => {
+      i18next.use(initReactI18next).init({ ...i18n });
+      return <I18nextProvider i18n={i18next}>{children}</I18nextProvider>;
+    },
+  });
 
-  expect(await screen.findByText("Remixez votre pizza")).toBeInTheDocument();
+  expect(await screen.findByText("Remix your pizza")).toBeInTheDocument();
 });
 
 test("ordering a pizza", async () => {
