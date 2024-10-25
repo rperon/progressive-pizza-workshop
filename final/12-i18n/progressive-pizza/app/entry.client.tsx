bbfc2c109949065ab772b22af66888f196d81be1
Rémi Peron <remi.peron@gmail.com>
2024-10-25 13:55:20 +0200

    i18n

---
+++ final/12-i18n/progressive-pizza/app/entry.client.tsx
@@ -0,0 +1,46 @@
+/**
+ * By default, Remix will handle hydrating your app on the client for you.
+ * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
+ * For more information, see https://remix.run/file-conventions/entry.client
+ */
+
+import { RemixBrowser } from "@remix-run/react";
+import i18next from "i18next";
+import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
+import { startTransition, StrictMode } from "react";
+import { hydrateRoot } from "react-dom/client";
+import { I18nextProvider, initReactI18next } from "react-i18next";
+import { getInitialNamespaces } from "remix-i18next/client";
+import { i18nConfig } from "./i18n";
+
+async function hydrate() {
+  await i18next
+    .use(initReactI18next)
+    .use(I18nextBrowserLanguageDetector)
+    .init({
+      ...i18nConfig,
+      ns: getInitialNamespaces(),
+      detection: {
+        order: ["htmlTag"],
+        caches: [],
+      },
+    });
+  startTransition(() => {
+    hydrateRoot(
+      document,
+      <I18nextProvider i18n={i18next}>
+        <StrictMode>
+          <RemixBrowser />
+        </StrictMode>
+      </I18nextProvider>
+    );
+  });
+}
+
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(hydrate);
+} else {
+  // Safari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  window.setTimeout(hydrate, 1);
+}
